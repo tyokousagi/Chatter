c3d5dff38e15cff887dd33180e0a905268f4adc6
tyokousagi <tyokousagi20022@gmail.com>
2025-03-09 12:12:09 +0900

    画像を追加

--- chatter/src/main/resources/data.sql
+++ chatter/src/main/resources/data.sql
@@ -8,4 +8,4 @@ INSERT INTO chat_rooms (room_name, created_at) VALUES
 INSERT INTO authentications (username,password,icon_url) VALUES 
 ('admin', '$2a$10$yGJhkW8baTD7ZiE1Q8Siq.Mo99q2Bj4qdoNk5aznEvr269j6R697u',NULL),
 ('sou','$2a$10$GgttR6BKPpIIQ/uVyqa7YuEYdXULvXZghfRz6.HezmaUwbJu.nmCm','/images/sou.png'),
-('naoko','$2a$10$DEOI4xODMOTC1fDx8ehagOvwZDaF2DsjeGRp5.FKeERx7RqK5h3wW',NULL);
+('naoko','$2a$10$DEOI4xODMOTC1fDx8ehagOvwZDaF2DsjeGRp5.FKeERx7RqK5h3wW','/images/naoko.png');
